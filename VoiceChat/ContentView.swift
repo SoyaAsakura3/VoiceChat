9eb89cf119aa8152cb1ef1d11fa18db9175117d7
SoyaAsakura <88998537+SoyaAsakura3@users.noreply.github.com>
2021-08-27 12:55:50 +0900

    Initial Commit

---
+++ VoiceChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VoiceChat
+//
+//  Created by 朝倉颯也 on 2021/08/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
